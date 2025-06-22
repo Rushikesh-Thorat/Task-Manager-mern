192b37712063d230196999f35dc53bec2c1bc351
rushikesh <rushithorst1234@gmail.com>
2025-06-23 03:04:15 +0530

    work on admin dashboard(structure and show task details done)

--- frontend/Task-Manager/src/utils/helper.js
+++ frontend/Task-Manager/src/utils/helper.js
@@ -2,3 +2,8 @@ export const validateEmail = (email) => {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(email);
 };
+
+export const addThousandsSeparator = (num) => {
+    if (num == null || isNaN(num)) return "";
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
